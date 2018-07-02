ec5cbcb08771c64b899e74b93848a02090b73f2b
Sebastian Alvarez <sebalvareztto@gmail.com>
2018-07-01 22:27:22 -0300

    Actualizacion: la DB funciona tras varias pruebas

--- SteamKeysMaven/src/main/java/Controller/EntityController.java
+++ SteamKeysMaven/src/main/java/Controller/EntityController.java
@@ -5,7 +5,9 @@
  */
 package Controller;
 
+import Controller.exceptions.NonexistentEntityException;
 import Model.Key;
+import java.util.Iterator;
 import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -17,12 +19,33 @@ public class EntityController {
     
     private static EntityManagerFactory emf;
     
-     public static void main(String[] args){
+     public static void main(String[] args) throws NonexistentEntityException{
         //Creamos el gestor de persistencia
         emf = Persistence.createEntityManagerFactory("SteamKeysPU");
         //manager = emf.createEntityManager();
-        
         KeyJpaController keysController = new KeyJpaController(emf);
+        
+        
+        //PRUEBA DE CREACION DE LLAVE
+        //Key k = new Key(63.99, "untradeable", "Revolver");
+        
+        //keysController.create(k);
+        //////////////////////////////////////////////////////
+        
+        //PRUEBA DE LISTAR LLAVES
+        List<Key> keys = (List<Key>) keysController.findKeyEntities();
+        Key key;
+        Iterator iter = keys.iterator();
+        while(iter.hasNext()){
+            key = (Key)iter.next(); 
+            System.out.println(key.getId() + " " + key.getKeyState() + " " + key.getKeyType());
+        }
+        //////////////////////////////////////////////////////
+       
+        //PRUEBA DE DESTRUCCION DE LLAVE
+        //keysController.destroy((long) 33);
+       
+        /////////////////////////////////////////////////////
         int cantidadkeys = keysController.getKeyCount(); 
        // List<Key> keys = (List<Key>) manager.createQuery("FROM key").getResultList();
         System.out.println("En esta base de datos hay " + cantidadkeys + " keys");
