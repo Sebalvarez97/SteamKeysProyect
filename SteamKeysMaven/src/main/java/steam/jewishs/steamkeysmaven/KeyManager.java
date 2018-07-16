286e0e2e752d6869b4e7873464ac65416f176ddb
Sebastian Alvarez <sebalvareztto@gmail.com>
2018-07-15 21:36:45 -0300

    Actualizacion

--- SteamKeysMaven/src/main/java/steam/jewishs/steamkeysmaven/KeyManager.java
+++ SteamKeysMaven/src/main/java/steam/jewishs/steamkeysmaven/KeyManager.java
@@ -116,6 +116,7 @@ public class KeyManager {
         returned = cal.get(Calendar.DAY_OF_MONTH) + "-" + cal.get(Calendar.MONTH) + "-" + cal.get(Calendar.YEAR);
         return returned;
     }
+    //DEVUELVE LA CANTIDAD DE LLAVES CON CIERTO ESTADO
     public static int CantWithState(TypeStateDTO dto) throws NonexistentEntityException{
         List<KeyDTO> keys = ListKeys();
         Iterator iter = keys.iterator();
